88c3b84576f64601a69108bbc28e4191a2e8d99c
iyjaa <izramulyaputra@gmail.com>
2026-07-12 07:57:46 +0700

    debugging

--- app/src/main/kotlin/com/jaaDev/mycompose/myfirstapp/MainActivity.kt
+++ app/src/main/kotlin/com/jaaDev/mycompose/myfirstapp/MainActivity.kt
@@ -4,29 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.ui.unit.dp
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.foundation.*
 import com.jaaDev.mycompose.myfirstapp.ui.theme.ComposeEmptyActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -97,7 +78,6 @@ fun Greeting(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeEmptyActivityTheme {

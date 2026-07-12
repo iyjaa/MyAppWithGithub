1fd06b68db1169f06496d23676862a2f9fa26ae6
iyjaa <izramulyaputra@gmail.com>
2026-07-12 11:46:10 +0700

    releases

--- app/src/main/kotlin/com/jaaDev/mycompose/myfirstapp/MainActivity.kt
+++ app/src/main/kotlin/com/jaaDev/mycompose/myfirstapp/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.foundation.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jaaDev.mycompose.myfirstapp.ui.theme.ComposeEmptyActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,12 +36,12 @@ fun Greeting(modifier: Modifier = Modifier) {
 
     var nama by remember { mutableStateOf("") }
     var sandi by remember { mutableStateOf("") }
-    var hasil by remember {mutableStateOf("") }
+    var hasil by remember { mutableStateOf("") }
 
-    Row(
+    Column(
         modifier = modifier.fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         OutlinedTextField(
             value = nama,
@@ -45,7 +50,7 @@ fun Greeting(modifier: Modifier = Modifier) {
             placeholder = { Text("masukkan nama") }
         )
 
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedTextField(
             value = sandi,
@@ -54,11 +59,11 @@ fun Greeting(modifier: Modifier = Modifier) {
             placeholder = { Text("masukkan password") }
         )
 
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
         Button(
-            onClick = { 
-                
+            onClick = {
+                hasil = "halo, $nama"
             },
             modifier = Modifier.padding(8.dp),
             enabled = true,
@@ -71,13 +76,17 @@ fun Greeting(modifier: Modifier = Modifier) {
         ) {
             Text("Login")
         }
-        hasil(
-            Text(text = "halo, $nama"),
-            color = Color.red
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = hasil,
+            color = Color.Red
         )
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeEmptyActivityTheme {
